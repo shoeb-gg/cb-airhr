60bd9510dd0ca596bdfaafe559644ca2f9c7aa57
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-05-06 20:06:27 +0600

    working on recruitment job post 1st tab

---
+++ src/app/pages/recruitment/job-post/job-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-post',
+  templateUrl: './job-post.component.html',
+  styleUrls: ['./job-post.component.scss']
+})
+export class JobPostComponent {
+
+}
--- src/app/pages/recruitment/recruitment-routing.module.ts
+++ src/app/pages/recruitment/recruitment-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RecruitmentComponent } from './recruitment.component';
+import { JobPostComponent } from './job-post/job-post.component';
 
 const routes: Routes = [
     {
         path: '',
         component: RecruitmentComponent,
+    },
+    {
+        path: 'job-post',
+        component: JobPostComponent,
     },
 ];
 
