ab26cb6594aa909e6932106145e3e5497dfcc8c8
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-05-17 20:20:22 +0600

    employee details & its tab UI

---
+++ src/app/pages/employee/employee-details/detail-tabs/detail-tabs.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-detail-tabs',
+    templateUrl: './detail-tabs.component.html',
+    styleUrls: ['./detail-tabs.component.scss'],
+})
+export class DetailTabsComponent {
+    public tabSelection: number = 1;
+
+    setSelectedTab(flag: boolean): void {
+        if (flag) {
+            this.tabSelection++;
+        }
+    }
+
+    setTabIndex(index: any) {
+        this.tabSelection = index.index;
+    }
+}
